fd41f4e400416323ce3cb76deef711178df76ce5
Patriciakloster08 <palhano.patricia@escola.pr.gov.br>
2025-08-21 09:11:03 -0300

    jkkjjk

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
 criaCartao(
     'fisioterapia',
     'O que é fisioterapia?',
-    'É uma ciência da saúde que estuda, previne e trata distúrbios do movimento e funcionalidade do corpo humanos, calor, frio, eletroter.'
+    'É uma ciência da saúde que estuda, previne e trata distúrbios do movimento e funcionalidade do corpo humano.'
 )
 
 criaCartao(
     'fisioterapia',
     'Qual o principal objetivo da fisioterapia?',
-    'Promover a reabilitação física, prevenir lesões e melhorar a qualidade de vida dos pacientes por meio de técnicas terapêuticas específicas.'
+    'Promover a reabilitação física, prevenir lesões e melhorar a qualidade de vida dos pacientes.'
 )
 
 criaCartao(
     'fisioterapia',
     'Como a fisioterapia ajuda em casos de AVC (Acidente Vascular Cerebral)?',
-    'Ajuda na recuperação de movimentos, melhora do equilíbrio, coordenação e força muscular, além de prevenir contraturas e promover a independência funcional.'
+    'Ajuda na recuperação de movimentos, melhora do equilíbrio, coordenação e força muscula.'
 )
     criaCartao(
     'fisioterapia',
     'O que é cinesioterapia?',
-    'É o uso de exercícios terapêuticos planejados para restaurar ou manter a função física e o movimento dos pacientes.'
+    'É o uso de exercícios planejados para restaurar ou manter a função física e o movimento dos pacientes.'
 )
